5dba30f56bfdc49c4fc853fa50a005a2f9e8aedd
Warren Green <warren.green84@gmail.com>
2019-12-01 21:33:27 +0000

    blah

--- scrape.py
+++ scrape.py
@@ -9,14 +9,15 @@ from selenium.webdriver.support.expected_conditions import presence_of_element_l
 #This example requires Selenium WebDriver 3.13 or newer
 with webdriver.Chrome() as driver:
     wait = WebDriverWait(driver, 10)
-    driver.get("https://shop.mango.com/gb/women/coats-quilted-coats/hood-quilted-coat_53048813.html?c=99&n=1&s=prendas_she.familia;15")
+    url = "https://shop.mango.com/gb/women/coats-quilted-coats/hood-quilted-coat_53048813.html?c=99&n=1&s=prendas_she.familia;15"
+    driver.get(url)
     driver.find_element_by_xpath("//form/div[2]/div/span").click()
     result = driver.find_element_by_xpath('/html/body/div[4]/div/form/div[2]/div[1]/main/div/div[3]/div[3]/form/div[2]/div/div/span[1]').text
     if result == 'XS - Not available I want it!':
-        print('OOS' + result)
-        requests.post(url='https://hooks.slack.com/services/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',data='{"text":"Out of stock!"}')
+        print(url +' OOS ' + result)
+        # requests.post(url='https://hooks.slack.com/services/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',data='{"text":"Out of stock!"}')
     else:
-        requests.post(url='https://hooks.slack.com/services/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',data='{"text":"In stock!"}')
+        requests.post(url='https://hooks.slack.com/services/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',data='{"text":"In stock! gotto https://shop.mango.com/gb/women/coats-quilted-coats/hood-quilted-coat_53048813.html?c=99&n=1&s=prendas_she.familia;15"}')
 
     # https://shop.mango.com/gb/women/coats-quilted-coats/hood-quilted-coat_53048813.html?c=99&n=1&s=prendas_she.familia;15
     
